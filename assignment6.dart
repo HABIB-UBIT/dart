d1a18f712035d30c41dfe2ca5bbc6143c2ed3eb2
Muhammad Habib <habibsahabubit@gmail.com>
2025-08-09 09:02:59 +0500

    assignment 6

--- assignment6.dart
+++ assignment6.dart
@@ -73,3 +73,66 @@
 
 //   print("Numbers greater than 5: $greaterThanFive");
 // }
+
+
+// // Q7.  Write a program that counts the
+// // number of vowels in a given string using a for loop and if-else condition.
+
+// void main(){
+//   String input = "Hello, World!";
+//   int vowelCount = 0;
+//   List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (int i = 0; i < input.length; i++) {
+//     String char = input[i].toLowerCase();
+//     if (vowels.contains(char)) {
+//       vowelCount++;
+//     }
+//   }
+
+//   print("Number of vowels in the string: $vowelCount");
+// }
+
+
+// // Q8. Write a Dart program that counts the number of digits in a given number using a while loop.
+// void main(){
+//   int number = 123456;
+//   int digitCount = 0;
+
+//   while (number > 0) {
+//     number ~/= 10; // Remove the last digit
+//     digitCount++;
+//   }
+
+//   print("Number of digits: $digitCount");
+// }
+
+
+// // Q9. Implement Dart code to generate a random password of a given length using a while loop.
+// import 'dart:math' as Math;
+// void main() {
+//   int passwordLength = 8;
+//   String characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+//   String password = '';
+//   int i = 0;
+
+//   while (i < passwordLength) {
+//     int randomIndex = (characters.length * Math.Random().nextDouble()).floor();
+//     password += characters[randomIndex];
+//     i++;
+//   }
+
+//   print("Generated password: $password");
+// }
+
+
+// // Q10. Create a Dart program that checks if a given string is empty or not using if-else statements.
+// void main(){
+//   String input = "Hello, World!";
+  
+//   if (input.isEmpty) {
+//     print("The string is empty.");
+//   } else {
+//     print("The string is not empty.");
+//   }
+// }
